b665dbedf75dcefd89afd80549f8543e4bc9261e
KR5T <sakarya1869@gmail.com>
2025-11-13 01:07:20 +0300

    Interfaces

--- GameTest.java
+++ GameTest.java
@@ -1,47 +1,44 @@
+interface IDestroyable {
+    void destroy(); 
+}
 abstract class Pickups {
-    public abstract void update();
+    public void update() {
+        System.out.println("Nesne dönüyor...");
+    }
 }
 
-class Coin extends Pickups {
-    @Override
-    public void update() { System.out.println("Coin dönüyor ve parlıyor..."); }
+class Coin extends Pickups implements IDestroyable {
+    @Override 
+    public void update() {
+        super.update();
+        System.out.println("   > Coin parlıyor!");
+    }
     
-    // Sadece Coin'e özel metot:
-    public void onPickup() { System.out.println("   >>> Coin alındı! +100 Puan!"); }
-}
-
-class Gem extends Pickups {
     @Override
-    public void update() { System.out.println("Mücevher dönüyor ve ışıldıyor..."); }
-    
-    // Sadece Gem'e özel metot:
-    public void onShatter() { System.out.println("   >>> Mücevher kırıldı!"); }
+    public void destroy() {
+        System.out.println("   >>> Coin yok edildi ve +100 Puan verildi!");
+    }
 }
 
+class Enemy implements IDestroyable {
+
+    @Override
+    public void destroy() {
+        System.out.println("   >>> Düşman yok edildi!");
+    }
+}
 public class GameTest {
     public static void main(String[] args) {
         
-        Pickups[] allPickups = new Pickups[3];
-        allPickups[0] = new Coin();
-        allPickups[1] = new Gem();
-        allPickups[2] = new Coin();
+        IDestroyable[] destroyableThings = new IDestroyable[2];
 
-        System.out.println("--- Polimorfik Döngü (instanceof ile) ---");
+        destroyableThings[0] = new Coin();
+        destroyableThings[1] = new Enemy();
+
+        System.out.println("--- Yok Edilebilirler Listesi ---");
         
-        for (Pickups p : allPickups) {
-            
-            // 1. Polimorfik çağrı (Herkes yapar)
-            p.update(); 
-
-            // 2. Tipe özel çağrı (instanceof ve Downcasting)
-            if (p instanceof Coin) {
-                Coin myCoin = (Coin) p; // Downcasting
-                myCoin.onPickup();
-            }
-            else if (p instanceof Gem) {
-                Gem myGem = (Gem) p; // Downcasting
-                myGem.onShatter();
-            }
+        for (IDestroyable item : destroyableThings) {
+            item.destroy(); 
         }
     }
 }
