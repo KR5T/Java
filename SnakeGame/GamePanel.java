b8caf1747981b105d9ae819bfc93b5134ab9ec78
KR5T <sakarya1869@gmail.com>
2025-12-29 23:57:24 +0300

    apple

--- SnakeGame/GamePanel.java
+++ SnakeGame/GamePanel.java
@@ -36,13 +36,20 @@ public class GamePanel extends JPanel implements ActionListener{
         timer = new Timer(DELAY,this);
     }
     public void draw(Graphics g){
-
+        for(int i= 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++){
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval((appleX), appleY, UNIT_SIZE, UNIT_SIZE);
     }
     public void newApple(){
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
+        draw(g);
     }
     public void move(){
 
